f5f5d721bb6f6a6a6e147c3839fd5e825545b536
Egor Philippov <ephilippov@gmail.com>
2017-12-07 15:11:31 -0800

    qr and barcode scanning via https://github.com/andrastoth/WebCodeCamJS

--- app/components/code-scanner.js
+++ app/components/code-scanner.js
@@ -1,3 +1,26 @@
-import Component from "@ember/component";
+import Component from '@ember/component';
+import { set } from '@ember/object';
+import $ from 'jquery';
 
-export default Component.extend({});
+export default Component.extend({
+  classNames: ['centered'],
+
+  didInsertElement() {
+    const onError = error => {
+      set(this, 'error', `${error.name} ${error.message ? '-' : ''} ${error.message}`);
+    };
+    const webCodeCamOptions = {
+      resultFunction: result => {
+        set(this, 'result', `Format: ${result.format} - Value: ${result.code}`);
+      },
+      getDevicesError: onError,
+      getUserMediaError: onError,
+      cameraError: onError
+    };
+
+    $('canvas#camera-view')
+      .WebCodeCamJQuery(webCodeCamOptions)
+      .data()
+      .plugin_WebCodeCamJQuery.play();
+  }
+});
--- config/environment.js
+++ config/environment.js
@@ -44,6 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/qr-scan-demo/';
     // here you can enable a production-specific feature
   }
 
